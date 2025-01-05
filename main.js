4f2ad6bcf6012937cf4101b0195595239447aebc
Anna <138930718+Kitsune1904@users.noreply.github.com>
2025-01-05 23:53:07 +0200

    add validation

--- main.js
+++ main.js
@@ -2,16 +2,12 @@ const form = document.querySelector('form');
 const formFile = document.getElementById("file");
 const uploadImg = document.querySelectorAll(".uploadFile");
 const ticketUser = document.querySelectorAll('.ticketUser');
-const ownerGit = document.querySelector('.ownerGit');
-const ticketEmail = document.querySelector('.ticketEmail');
 const ticketContainer = document.querySelector('.ticket-container');
 const formContainer = document.querySelector('.form-container');
-const ticketNum = document.querySelector('.ticketNum');
-const fileControl = document.querySelector('.fileControl');
-const fileLabel = document.getElementById("fileLabel");
-const fileMessage = document.querySelector(".fileMessage");
 const fileErr = document.querySelector(".fileErr");
 const submitBtn = document.getElementById("submitBtn");
+const errorMsgs = document.querySelectorAll(".error");
+const inputs = document.querySelectorAll('input');
 
 let counter = 0;
 
@@ -24,7 +20,8 @@ const generateNumber = () => {
     return '#' + strNum;
 }
 
-const showError = (isShow) => {
+const showFileError = (isShow) => {
+    const fileMessage = document.querySelector(".fileMessage");
     if(isShow) {
         fileMessage.style.display = 'none';
         fileErr.style.display = 'flex';
@@ -39,9 +36,9 @@ const showError = (isShow) => {
 const addImage = (fileInput, container) => {
     const file = fileInput.files[0];
     const maxFileSize = 500 * 1024;
-    showError(false)
+    showFileError(false)
     if (file.size > maxFileSize) {
-        showError(true)
+        showFileError(true)
     }
 
     const reader = new FileReader();
@@ -55,7 +52,16 @@ const addImage = (fileInput, container) => {
     reader.readAsDataURL(file);
 }
 
+const showErrorMsgs = (message) => {
+    errorMsgs[message].style.display = 'block';
+    inputs[message].classList.add('empty');
+
+}
+
 formFile.addEventListener('change', () => {
+    const fileControl = document.querySelector('.fileControl');
+    const fileLabel = document.getElementById("fileLabel");
+
     addImage(formFile, uploadImg)
     fileControl.innerHTML = '<button type="button" id="removeBtn">Remove image</button> <button type="button" id="changeBtn">Change image</button>';
     fileLabel.setAttribute('for', 'nothing');
@@ -65,14 +71,14 @@ formFile.addEventListener('change', () => {
     const removeBtn = document.getElementById("removeBtn");
     const changeBtn = document.getElementById("changeBtn");
 
-    removeBtn.addEventListener('click', (e) => {
+    removeBtn.addEventListener('click', () => {
         uploadImg.forEach((image) => {
             image.src = './assets/images/person-svgrepo-com.svg';
             image.alt = 'person image';
             image.style.padding = '0.625rem';
         })
         formFile.value = "";
-        showError(false)
+        showFileError(false)
     })
     changeBtn.addEventListener('click', () => {
         formFile.click();
@@ -82,19 +88,45 @@ formFile.addEventListener('change', () => {
 
 
 
+inputs.forEach((input) => {
+    input.addEventListener('input', (e) => {
+        const errorElement = e.target.nextElementSibling;
+        const allFilled = Array.from(inputs).slice(1).every(inp => inp.value.trim() !== '');
+        input.classList.remove('empty');
+        errorElement.style.display = 'none';
+        submitBtn.disabled = !allFilled;
+    })
+})
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    const fields = ['name', 'email', 'git'];
     const formData = new FormData(form);
-    const file = formData.get('file');
-    const name = formData.get('name');
-    const email = formData.get('email');
-    const git = formData.get('git');
-    ticketUser.forEach(user => {
-        user.innerText = name;
+    let isValid = true;
+    const templates = {
+        name: /^[a-zA-Z0-9 -]*$/,
+        email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        git: /^@[a-zA-Z0-9 _]*$/
+    }
+
+    fields.forEach((field, index) => {
+        const input = form.querySelector(`[name="${field}"]`);
+        const value = formData.get(field).trim();
+        if (!templates[field].test(value)) {
+            input.value = '';
+            showErrorMsgs(index + 1);
+            isValid = false;
+        }
     })
-    ownerGit.innerText = git;
-    ticketEmail.innerText = email;
+
+    if (!isValid) return;
+
+    ticketUser.forEach(user => {
+        user.innerText = formData.get('name').trim();;
+    })
+    document.querySelector('.ownerGit').innerText = formData.get('git').trim();
+    document.querySelector('.ticketEmail').innerText = formData.get('email').trim();
     ticketContainer.style.display = 'block';
     formContainer.style.display = 'none';
-    ticketNum.innerText = generateNumber();
+    document.querySelector('.ticketNum').innerText = generateNumber();
 })
